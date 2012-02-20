77661a6c3eddf6a07a26dea465d7cc7b0ed21f4b
Ryan Brodkin <ryan@glabstudios.com>
2012-02-20 05:32:39 -0800

    Updating submodules. Hostname now used as default base URL.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= "https://".$_SERVER['SERVER_NAME'];
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
