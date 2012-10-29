6797c8d257cf5de910a8086e579246ceae142d96
Ryan Brodkin <ryan@brodkinca.com>
2012-10-29 09:37:54 -0700

    Adjusting file names to avoid collisions.

--- application/controllers/webhook.php
+++ application/controllers/webhook.php
@@ -40,7 +40,7 @@ class webhook extends CI_Controller
             $out['repo'] = $data->repository->name;
             $out['branch'] = (strpos($data->ref, 'refs/heads/') === 0) ? substr($data->ref, 11) : false;
 
-            $file_name = sha1('$data->repository->url').'.json';
+            $file_name = $out['user'].'_'.$out['repo'].'_'.$out['hash'].$out['branch'].'.json';
 
             $this->load->helper('file');
             write_file(FCPATH.'/webhook_data/'.$file_name, json_encode($out));
