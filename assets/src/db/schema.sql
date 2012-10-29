90d11fbb2437295e327ac2f535aeb21a15407901
Ryan Brodkin <ryan@brodkinca.com>
2012-10-29 08:30:14 -0700

    Added database schema

---
+++ assets/src/db/schema.sql
@@ -0,0 +1,49 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `access_token` CHAR(40) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `access_key_UNIQUE` (`access_token` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `repos`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `repos` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_repos_users_idx` (`user_id` ASC) ,
+  CONSTRAINT `fk_repos_users`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `users` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sessions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `sessions` (
+  `session_id` VARCHAR(40) NOT NULL DEFAULT '0' ,
+  `ip_address` VARCHAR(45) NOT NULL DEFAULT '0' ,
+  `user_agent` VARCHAR(120) NOT NULL ,
+  `last_activity` INT(10) UNSIGNED NOT NULL DEFAULT 0 ,
+  `user_data` TEXT NOT NULL ,
+  PRIMARY KEY (`session_id`) ,
+  INDEX `last_activity_idx` (`last_activity` ASC) );
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
