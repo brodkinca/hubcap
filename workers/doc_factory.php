5b19636206298345c720418422ba28ce45fcbd0e
Ryan Brodkin <ryan@brodkinca.com>
2012-12-17 15:03:21 -0800

    Fixing bug in which shared working directories are not symlinked in time for filesystem access.

--- workers/doc_factory.php
+++ workers/doc_factory.php
@@ -15,17 +15,14 @@
  * @link      http://github.com/brodkinca/hubcap
  */
 
+echo "Starting worker...\n";
+sleep(30);
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 /* Setup Logger */
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-
-system('ls -l '.__DIR__.'/../');
-echo "zzzzZZZzzz\n";
-sleep(30);
-system('ls -l '.__DIR__.'/../');
-echo "\n";
 
 $log_path = realpath(__DIR__.'/../hubcap_logs/').'/workers.log';
 touch($log_path);
